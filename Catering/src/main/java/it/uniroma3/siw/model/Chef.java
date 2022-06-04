8887aa725a60773f1d0eeb50bfb10bcf35e3e27a
Skesky <kevin03023@gmail.com>
2022-06-04 17:32:51 +0200

    created model classes

---
+++ Catering/src/main/java/it/uniroma3/siw/model/Chef.java
@@ -0,0 +1,68 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Chef {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	private String cognome;
+	
+	private String nazionalitá;
+	
+	@OneToMany
+	private List<Buffet> buffets;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getNazionalitá() {
+		return nazionalitá;
+	}
+
+	public void setNazionalitá(String nazionalitá) {
+		this.nazionalitá = nazionalitá;
+	}
+
+	public List<Buffet> getBuffets() {
+		return buffets;
+	}
+
+	public void setBuffets(List<Buffet> buffets) {
+		this.buffets = buffets;
+	}
+	
+	
+}
---
+++ Catering/src/main/java/it/uniroma3/siw/validator/IngredienteValidator.java
@@ -0,0 +1,20 @@
+package it.uniroma3.siw.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class IngredienteValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
